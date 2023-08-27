cb7266584ab4c82e3347520f49bbe6f9a22c0878
Julian Montague <juliangm@stud.ntnu.no>
2023-08-27 18:05:02 +0200

    Add solution for exercise 1

--- src/exercises/exercise-1/solution.ts
+++ src/exercises/exercise-1/solution.ts
@@ -1,8 +1,23 @@
 export function sortMatrix(matrix: number[][][]): number[][][] {    
-    // You need to update this array using the matrix
-    const sortedMatrix: number[][][] = []
-    
-    // Your Solution here:
-    
-    return sortedMatrix
+    let vector = matrix.flat();
+    vector.sort(arrayCompare);
+    return createMatrix(vector);
+}
+
+function sumArray(array: number[]): number {
+    return array[0] + array[1] + array[2];
+}
+
+function arrayCompare(a: number[], b: number[]): number {
+    return sumArray(a) - sumArray(b);
+}
+
+function createMatrix(array: number[][]): number[][][] {
+    // turn the vector of arrays into a matrix of arrays,
+    // according to the exercise description
+    return [
+        [array[0], array[1], array[2]],
+        [array[3], array[4], array[5]],
+        [array[6], array[7], array[8]]
+    ];
 }
